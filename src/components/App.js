5da8a59318e33a6ff576f6f3114cc5aa6d473829
Dipjyoti Metia <dipjyoti.metia@beteasy.com.au>
2019-07-02 17:11:35 +1000

    react router

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,18 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import CoursesPage from './CoursesPage';
 import Header from './common/Header';
+import { Route } from 'react-router-dom';
 
 function App() {
-    function getPage() {
-        const route = window.location.pathname;
-        if (route === "/courses") return <CoursesPage />
-        if (route === "/about") return <AboutPage />
-        return <HomePage />
-    }
+
     return (
         <div>
             <Header />
-            {getPage()}
+            <Route path="/" exact component={HomePage} />
+            <Route path="/courses" component={CoursesPage} />
+            <Route path="/about" component={AboutPage} />
         </div>
-        );
+    );
 }
 
 export default App;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
@@ -6,6 +7,7 @@ function HomePage() {
             <h1>Pluralsight Administration</h1>
             <p>React, Flux react router for ultra responsive web</p>
             <a href="/about">About</a>
+            <Link to="about" className="btn btn-primary"></Link>
         </div>
     );
 }
